25f49f599eed9f0cceb94b780f0e59f71cbb429e
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-29 19:32:03 -0300

    agregue la instalacion del paqute

--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker run -ti -d --name login-consent -v $(pwd)/src:/src -p 5000:5000 -p 5001:5001 -p 5002:5002 -p 5003:5003 --env-file /home/pablo/gitlab/fce/produccion/login-consent login-consent
+sudo docker run -ti -d --name login-consent -v $(pwd)/src:/src -p 5000:5000 -p 5001:5001 -p 5002:5002 -p 5003:5003 --env-file $HOME/gitlab/fce/produccion/login-consent login-consent
 sudo docker exec -t login-consent bash instalar.sh
 
